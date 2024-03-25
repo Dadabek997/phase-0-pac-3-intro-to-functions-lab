e24175359a8566ecce5f0d88f84eb01a42f3a1cd
Dadabek Rajabov <barajabov@gmail.com>
2024-03-25 15:08:38 -0400

    Adds index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+function shout(string) {
+    return string;
+}
+
+"Hello".toUpperCase();
+
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string;
+}
+
+"Hello".toLowerCase();
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+
+function logShout(hello) {
+    console.log(hello.toUpperCase());
+}
+
+
+function logWhisper(Hello) {
+    console.log(Hello.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate() {
+    console.log(sayHiToHeadphonedRoommate);
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if(string.toLowerCase() === string) {
+        return 'I can\'t hear you!';
+    }
+    else if(string.toUpperCase() === string) { 
+        return 'YES INDEED!';
+    }
+    if("Let\'s have dinner together.") {
+        return 'I would love to!';
+    }
+
+}
+
